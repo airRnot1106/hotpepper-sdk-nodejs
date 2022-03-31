aaf96e2d4d7b0d1828f158a399bdbcc897da9ee4
airRnot1106 <yngw.rintaro@gmail.com>
2022-03-31 14:18:44 +0900

    largeServiceAreaの実装

---
+++ src/api/largeServiceArea.ts
@@ -0,0 +1,40 @@
+import fetch from 'node-fetch';
+
+import { KeyManager } from '../keyManager';
+import { ENDPOINT, HotPepperResponse, isSuccessfulResponse, ResponseField } from './apiBase';
+
+interface ResponseLargeServiceArea {
+    large_service_area: {
+        code: string;
+        name: string;
+    }[];
+}
+
+export class LargeServiceArea {
+    private _URL = `${ENDPOINT}/large_service_area/v1`;
+    private _keyManager = KeyManager.instance;
+
+    constructor() {}
+
+    async search(): Promise<
+        HotPepperResponse<ResponseField<ResponseLargeServiceArea>>
+    > {
+        const params = new URLSearchParams({
+            key: this._keyManager.apiKey,
+            format: 'json',
+        });
+        const res = await fetch(`${this._URL}?${params}`);
+        const json = <ResponseField<ResponseLargeServiceArea>>await res.json();
+        if (isSuccessfulResponse(json))
+            return {
+                status: 200,
+                result: json.results.large_service_area,
+                rawJson: json,
+            };
+        return {
+            status: json.results.error[0].code,
+            error: json.results.error[0].message,
+            rawJson: json,
+        };
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import { Budget } from './api/budget';
+import { LargeServiceArea } from './api/largeServiceArea';
 
 export { setApiKey } from './setApiKey';
 
 export const budget = () => {
     return new Budget();
+};
+
+export const largeServiceArea = () => {
+    return new LargeServiceArea();
 };
