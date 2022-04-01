900323c792855360c466af118adda41fe970fe54
airRnot1106 <yngw.rintaro@gmail.com>
2022-04-01 11:50:32 +0900

    ピリオドを追加

--- src/index.ts
+++ src/index.ts
@@ -14,84 +14,84 @@ import { SpecialCategory } from './api/specialCategory';
 export { setApiKey } from './setApiKey';
 
 /**
- * Dinner Budget Master API for Search
+ * Dinner Budget Master API for Search.
  *
  * @returns {any} {Budget}
  */
 export const budget = (): Budget => new Budget();
 
 /**
- * Large Service Area Master API
+ * Large Service Area Master API.
  *
  * @returns {any} {LargeServiceArea}
  */
 export const largeServiceArea = (): LargeServiceArea => new LargeServiceArea();
 
 /**
- * Service Area Master API
+ * Service Area Master API.
  *
  * @returns {any} {ServiceArea}
  */
 export const serviceArea = (): ServiceArea => new ServiceArea();
 
 /**
- * Large Area Master API
+ * Large Area Master API.
  *
  * @returns {any} {LargeArea}
  */
 export const largeArea = (): LargeArea => new LargeArea();
 
 /**
- * Middle Area Master API
+ * Middle Area Master API.
  *
  * @returns {any} {MiddleArea}
  */
 export const middleArea = (): MiddleArea => new MiddleArea();
 
 /**
- * Small Area Master API
+ * Small Area Master API.
  *
  * @returns {any} {SmallArea}
  */
 export const smallArea = (): SmallArea => new SmallArea();
 
 /**
- * Genre Master API
+ * Genre Master API.
  *
  * @returns {any} {Genre}
  */
 export const genre = (): Genre => new Genre();
 
 /**
- * Credit Card Master API
+ * Credit Card Master API.
  *
  * @returns {any} {CreditCard}
  */
 export const creditCard = (): CreditCard => new CreditCard();
 
 /**
- * Special Category Master API
+ * Special Category Master API.
  *
  * @returns {any} {SpecialCategory}
  */
 export const specialCategory = (): SpecialCategory => new SpecialCategory();
 
 /**
- * Special Master API
+ * Special Master API.
  *
  * @returns {any} {Special}
  */
 export const special = (): Special => new Special();
 
 /**
- * Gourmet Search API
+ * Gourmet Search API.
  *
  * @returns {any} {Gourmet}
  */
 export const gourmet = (): Gourmet => new Gourmet();
 
 /**
- * Shop Search API
+ * Shop Search API.
  *
  * @returns {any} {Shop}
  */
