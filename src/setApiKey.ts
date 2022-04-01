5b1fafb9d7389850ac34a25f1ab12428a892e51e
airRnot1106 <yngw.rintaro@gmail.com>
2022-04-01 11:44:22 +0900

    jsdocを追加

--- src/setApiKey.ts
+++ src/setApiKey.ts
@@ -1,5 +1,11 @@
 import { KeyManager } from './keyManager';
 
+/**
+ * Set the API key. This is required for all API calls. API keys can be
+ * generated at https://webservice.recruit.co.jp/register/
+ *
+ * @param {string} apiKey
+ */
 export const setApiKey = (apiKey: string) => {
     KeyManager.instance.apiKey = apiKey;
 };
